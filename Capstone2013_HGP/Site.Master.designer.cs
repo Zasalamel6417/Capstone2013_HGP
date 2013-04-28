2bdb6006f354f0b4a243342ae36bc8a7398e635d
Chris Bell <zelda6417@gmail.com>
2013-04-28 18:50:07 -0500

    Coorected header image and added social icons ton image folder

--- Capstone2013_HGP/Site.Master.designer.cs
+++ Capstone2013_HGP/Site.Master.designer.cs
@@ -7,38 +7,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Capstone2013_HGP
-{
-
-
-	public partial class SiteMaster
-	{
-
-		/// <summary>
-		/// HeadContent control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
-		/// <summary>
-		/// FeaturedContent control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
-
-		/// <summary>
-		/// MainContent control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-	}
+namespace Capstone2013_HGP {
+    
+    
+    public partial class SiteMaster {
+        
+        /// <summary>
+        /// HeadContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        
+        /// <summary>
+        /// FeaturedContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
+        
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+    }
 }
