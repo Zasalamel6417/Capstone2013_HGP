5b14a2350bc332722dda0db3e5ec4b1042d101d5
Abdullahi Ibrahim <Abdullibrahim@rocketmail.com>
2013-05-05 02:34:08 -0500

    Added Data To the Database
    Tables: Artist, Venue, Address, Event.

---
+++ NewDataAdded.sql
@@ -0,0 +1,7 @@
+﻿select * from Event
+
+insert into StateProvince
+values('80', '10', 'California')
+
+insert into Event
+values(20, 3, 'No Shoes No Shirt', '08/20/2013', '08/20/2013', '05:00 PM', '11:00 PM')
