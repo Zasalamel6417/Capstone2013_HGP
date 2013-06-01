fceafb964c528f7e12954b4b1476398b934438da
Joshua Moreland <jmoreland91@gmail.com>
2013-05-31 20:33:52 -0500

    hi

--- Capstone2013_HGP/Order.aspx.cs
+++ Capstone2013_HGP/Order.aspx.cs
@@ -64,14 +64,18 @@ namespace Capstone2013_HGP
             //Get Logged in user
             MembershipUser mu = Membership.GetUser();
 			SqlConnection sqlConn = new SqlConnection(connStr);
-			if (Request.QueryString["cust"] != null)
-			{
-				SqlCommand sqlEmail = new SqlCommand("SELECT email FROM Customer WHERE custID=@id", sqlConn);
-				sqlEmail.Parameters.Add(new SqlParameter("@id", Request.QueryString["cust"]));
-				sqlConn.Open();
-				custEmail = (string)sqlEmail.ExecuteScalar();
-				sqlConn.Close();
-			}
+            if (Request.QueryString["cust"] != null)
+            {
+                SqlCommand sqlEmail = new SqlCommand("SELECT email FROM Customer WHERE custID=@id", sqlConn);
+                sqlEmail.Parameters.Add(new SqlParameter("@id", Request.QueryString["cust"]));
+                sqlConn.Open();
+                custEmail = (string)sqlEmail.ExecuteScalar();
+                sqlConn.Close();
+            }
+            else
+            {
+                custEmail = mu.Email;
+            }
 			
             //If the page is not a postback, Get the time check if it's less than 7 days.
             //If so, don't display the pickup check box.
